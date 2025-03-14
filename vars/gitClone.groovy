b3b8fa714e9e92c4c9c5712e56d9f42489a077b3
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-15 01:46:40 +0530

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,13 +1,7 @@
-def call(String repoUrl, String branch, String credentialsId = '') {
-    echo "Cloning repository from ${repoUrl}, branch: ${branch}"
-    
-    if (credentialsId) {
-        echo "Using credentials ID: ${credentialsId}"
-        git branch: branch, 
-            url: repoUrl, 
-            credentialsId: credentialsId
-    } else {
-        git branch: branch, 
-            url: repoUrl
-    }
-}  
+def call(String branch, String repo, String credentialsId) {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: repo, credentialsId: credentialsId]]
+    ])
+}
