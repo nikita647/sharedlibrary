7d6e854da80bc9350de939c7c40fd15d72873316
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-15 23:55:59 +0530

    Update bugAnalysis.groovy

--- vars/bugAnalysis.groovy
+++ vars/bugAnalysis.groovy
@@ -1,10 +1,11 @@
 def call(String projectKey_name ) {
-                withSonarQubeEnv('sonar') {
+                   echo "Using SonarQube Project Key: ${projectKey_name}"
+            withSonarQubeEnv('sonar') {
                 withCredentials([string(credentialsId: 'sonartoken', variable: 'SONARQUBE_TOKEN')]) {
-                sh '''
-              sonar-scanner \
-             -Dsonar.projectKey="${projectKey_name}" \
-             -Dsonar.token="${SONARQUBE_TOKEN}"
-                '''
-                
+                    sh """
+                        /opt/sonar-scanner/bin/sonar-scanner \
+                        -Dsonar.projectKey=${projectKey_name} \
+                        -Dsonar.sources=. \
+                        -Dsonar.token=${SONARQUBE_TOKEN}
+                    """
     }
