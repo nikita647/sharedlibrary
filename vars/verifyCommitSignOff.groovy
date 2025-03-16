be5a81d4053d783602a10d91a6984d3d57ee573f
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-16 14:03:16 +0530

    Update and rename verifyCommitSignOff.groovy groovy Copy code to verifyCommitSignOff.groovy

--- vars/verifyCommitSignOff.groovy
+++ vars/verifyCommitSignOff.groovy
@@ -7,5 +7,3 @@ def call() {
         echo "All commits have been signed off."
     }
 }
-def gitCommit = sh(script: 'git log -1 --pretty=%an', returnStdout: true).trim()
-                    def gitCommitMsg = sh(script: 'git log -1 --pretty=%B', returnStdout: true).trim()
