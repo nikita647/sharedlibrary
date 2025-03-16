d18aee2cb6a9be36e29dd7f9d5927447edd77ba3
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-16 13:45:57 +0530

    Create postBuildFailure.groovy

---
+++ vars/postBuildFailure.groovy
@@ -0,0 +1,22 @@
+def call(String emailRecipient, String slackChannel) {
+    echo "Sending failure notifications..."
+    
+    emailext body: """Hello,
+
+The Jenkins pipeline ${env.JOB_NAME} has failed on Build #${env.BUILD_NUMBER}.
+
+Job Details:
+- Job Name: ${env.JOB_NAME}  
+- Build Number: ${env.BUILD_NUMBER}  
+- Build URL: ${env.BUILD_URL}  
+
+Regards,  
+Jenkins CI
+""", 
+            subject: "Job Name: ${env.JOB_NAME}, Build Number: ${env.BUILD_NUMBER}", 
+            to: emailRecipient
+    
+    slackSend(channel: slackChannel, 
+              message: "Build Failed: JOB-Name:- ${env.JOB_NAME} Build_No.:- ${env.BUILD_NUMBER} & Build-URL:- ${env.BUILD_URL}", 
+              tokenCredentialId: 'slack')
+}
