a31075b633cd314e1f0a1bf6030e47ff96b903a2
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-16 21:35:44 +0530

    Update verifyCommitSignOff.groovy

--- vars/verifyCommitSignOff.groovy
+++ vars/verifyCommitSignOff.groovy
@@ -1,10 +1,14 @@
 def call() {
-    sh  'git config commit.gpgsign false'
-      def unsignedCommits = sh(script: '''
-                        git log --pretty=format:"%h - %an: %s" --show-signature | grep -i -v "signed-off-by"
-                    ''', returnStdout: true).trim()
-
-                    if (unsignedCommits) {
-                        error "The following commits are missing a sign-off:\n${unsignedCommits}"
-                    }
+    // Ensure the config is applied globally
+    sh 'git config --global commit.gpgsign false'
+    // Confirm the change took effect
+    sh 'git config --get commit.gpgsign'
+    // Identify unsigned commits
+    def unsignedCommits = sh(script: '''
+        git log --pretty=format:"%h - %an: %s" --show-signature | grep -i -v "signed-off-by" || true
+    ''', returnStdout: true).trim()
+    // Fail the build if unsigned commits are found
+    if (unsignedCommits) {
+        error "The following commits are missing a sign-off:\n${unsignedCommits}"
+    }
 }
